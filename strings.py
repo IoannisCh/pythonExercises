0f9b51d1c6317cff86c39d4a7a0f939e8040eae8
Yiannis <yiannis@pop-os.localdomain>
2023-03-26 13:13:24 +0100

    first commit

---
+++ strings.py
@@ -0,0 +1,17 @@
+numbers = []
+strings = []
+names = ["John", "Joe", "Jane£"]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append(1)
+strings.append(2)
+strings.append(3)
+
+second_name = names[1]
+
+print(numbers)
+print(names)
+print("The second name on the list is $s" % second_name)
